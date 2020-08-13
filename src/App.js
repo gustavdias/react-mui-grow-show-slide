44b5770c3eb2f79ce0319660e54be7fa4aab36c4
gustavdias <gustav.almd@gmail.com>
2020-08-13 20:40:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Zoom from './Zoom';
+import Grow from './Grow';
+import Slide from './Slide'
+
+
+function App() {
+  return (
+    <>
+<Zoom/>
+<Grow/>
+<Slide/>
+</>
+  );
+}
+
+export default App;
